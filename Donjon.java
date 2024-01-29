1c7a5704ce6bc0b7719c42c58d9df0830458869e
Longueville_Francois <mrlongueville@gmail.com>
2024-01-29 14:22:05 +0100

    add donjon class

---
+++ Donjon.java
@@ -0,0 +1,23 @@
+
+public class Donjon {
+
+	public Donjon(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void generateDonjon() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

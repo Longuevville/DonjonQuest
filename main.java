81c60ac1c1225bad3a9404a33313180daecffcd5
Longuevville <mrlongueville@proton.me>
2024-03-24 17:50:48 +0100

    systeme de fouilles

--- main.java
+++ main.java
@@ -1,46 +1,28 @@
 import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-	  public String Donjon;
+        System.out.println("Veux-tu rentrer dans le donjon aventurier ? (oui/non)");
 
-	    public static void main(String[] args) {
-	    	//test
-	    	//Affiché un messagge pour demander au joueur si il veut jouer
-	    	//Si oui appelé la classe room
-	    	//Si non quitté le programme
-	    	
-	    //creation obt scanner et joueur
-	    	Scanner scan = new Scanner(System.in);
-	    	Player player = new Player();
-	    	room room = new room();
-	    	
-	    // message de bienvenue lugubre
-	    	System.out.println("Veux tu rentrer dans le donjon aventurier ?  (oui/non");
-	    	
-	    // lire reponse	
-	    	String reponse = scan.next();
-	    	
-	    // si oui appelé classe room
-	    	
-	    	if ( reponse.equalsIgnoreCase("oui")) {
-	  
-	    		
-	    ////creation d'une instance d'une piece
-	    		
-	    		
-	    		room.enter();
-	    		
-	    //si joueur ne vveux pas jouer
-	    	}else if(reponse.equalsIgnoreCase("non")) {
-	    		System.out.println("Vous manquez de courage et repartez");
-	    		
-	    //Fermeture du jeu
-	    		System.exit(0);
-	    		scan.close();
-	   //verif 		
-	    		
-	    		
-	    	}
-	    }
-	
+        String reponse = scan.next();
+
+        if (reponse.equalsIgnoreCase("oui")) {
+            room.enter();
+            System.out.println("Voulez-vous fouiller le mobilier ? (oui/non)");
+            String fouillerReponse = scan.next();
+            if (fouillerReponse.equalsIgnoreCase("oui")) {
+                room.fouillerMobilier();
+            } else {
+                System.out.println("Vous décidez de ne pas fouiller le mobilier.");
+            }
+        } else if (reponse.equalsIgnoreCase("non")) {
+            System.out.println("Vous manquez de courage et repartez.");
+        } else {
+            System.out.println("Réponse invalide. Fin du programme.");
+        }
+
+        scan.close();
+    }
 }
--- room.java
+++ room.java
@@ -1,32 +1,28 @@
-import java.util.Scanner;
-import java.util.Random;
 import java.util.Random;
 
-public class room extends main {
-    
+public class room {
     // Les objets dans les pièces
     static class Mobilier {
         private String nom;
-        
+
         public Mobilier(String nom) {
             this.nom = nom;
         }
-        
+
         public String getNom() {
             return nom;
         }
     }
-    
+
     // Tableau de mobilier
     static int rows = 3; // les lignes
     static int cols = 3; // les colonnes
-    
-    public static void main(String[] args) {
+
+    public static void enter() {
+        System.out.println("Vous vous retrouvez prisonnier dans une salle sombre");
         // Déclaration du tableau de mobilier
         Mobilier[][] tableauMobilier = generateTableauMobilier(rows, cols);
-        
-        
-        
+
         // Afficher le tableau de mobilier
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -40,14 +36,14 @@ public class room extends main {
             System.out.println();
         }
     }
-    
+
     // Méthode pour générer un tableau de mobilier
     public static Mobilier[][] generateTableauMobilier(int rows, int cols) {
         Random random = new Random();
         Mobilier[][] tableauMobilier = new Mobilier[rows][cols];
-        
-        String[] typesDeMobilier = {"Chaise", "Table",  "Lampe", "Armoire", "Bureau"};
-        
+
+        String[] typesDeMobilier = {"Chaise", "Table", "Lampe", "Armoire", "Bureau"};
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (random.nextBoolean()) { // Probabilité de 50% d'avoir un mobilier dans une case du tableau
@@ -57,11 +53,14 @@ public class room extends main {
                 }
             }
         }
-        
+
         return tableauMobilier;
     }
-    
-    public static void enter() {
-        System.out.println("Vous vous retrouvez prisonnier dans une salle sombre");
+
+    // Méthode pour fouiller le mobilier
+    public static void fouillerMobilier() {
+        System.out.println("Vous commencez à fouiller le mobilier...");
+        // Ajoutez ici votre logique pour fouiller le mobilier
+        // Par exemple, vous pouvez parcourir le tableau de mobilier et afficher les détails de chaque objet
     }
 }

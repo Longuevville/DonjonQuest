c12cfc737e4c94f57f74885ca3128ea72ac8a4b9
Longueville <155456691+Longuevville@users.noreply.github.com>
2024-04-10 17:58:05 +0200

    Update Donjon.java

--- Donjon.java
+++ Donjon.java
@@ -1,23 +1,64 @@
+vimport java.util.Random;
+import java.util.Scanner;
 
 public class Donjon {
+    // Les objets qui se trouvent dans les pièces
+    static class Mobilier {
+        private String nom;
 
-	public Donjon(String string) {
-		// TODO Auto-generated constructor stub
-	}
+        public Mobilier(String nom) {
+            this.nom = nom;
+        }
 
-	public void generateDonjon() {
-		// TODO Auto-generated method stub
-		
-	}
+        public String getNom() {
+            return nom;
+        }
+    }
 
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    // Tableau des pièces
+    private room[] pieces;
 
-	public void setName(String string) {
-		// TODO Auto-generated method stub
-		
-	}
+    public Donjon() {
+        pieces = new room[8];
+        for (int i = 0; i < 8; i++) {
+            pieces[i] = new room(generateTableauMobilier(3, 3));
+        }
+    }
 
+    // Méthode pour entrer dans une pièce
+    public void entrerPiece(int numeroPiece) {
+        System.out.println("Vous entrez dans la pièce " + numeroPiece + ".");
+        pieces[numeroPiece - 1].afficherPiece();
+        
+        // Proposer au joueur de fouiller la pièce
+        System.out.println("Voulez-vous fouiller cette pièce ? (oui/non)");
+        Scanner scan = new Scanner(System.in);
+        String reponse = scan.next();
+        if (reponse.equalsIgnoreCase("oui")) {
+            pieces[numeroPiece - 1].fouillerPiece();
+        } else {
+            System.out.println("Vous décidez de ne pas fouiller la pièce.");
+        }
+        scan.close();
+    }
+
+    // Méthode pour générer un tableau de mobilier
+    private Mobilier[][] generateTableauMobilier(int rows, int cols) {
+        Random random = new Random();
+        Mobilier[][] tableauMobilier = new Mobilier[rows][cols];
+
+        String[] typesDeMobilier = {"Chaise", "Table", "Lampe", "Armoire", "Bureau"};
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (random.nextBoolean()) { // Probabilité de 50% d'avoir un mobilier dans une case du tableau
+                    int index = random.nextInt(typesDeMobilier.length);
+                    String nomMobilier = typesDeMobilier[index];
+                    tableauMobilier[i][j] = new Mobilier(nomMobilier);
+                }
+            }
+        }
+
+        return tableauMobilier;
+    }
 }

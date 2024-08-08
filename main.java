5459429388a036140002f4affcd11ea85b43e11e
Longueville <155456691+Longuevville@users.noreply.github.com>
2024-08-08 21:41:46 +0200

    Update main.java
    Include Printer method

--- main.java
+++ main.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	 public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Bienvenue dans le donjon aventurier !");
+	        // Message de bienvenue
+	        Printer.printMessage("Bienvenue dans le donjon aventurier !");
 
-        // Création du donjon
-        Donjon donjon = new Donjon();
+	        // Création du donjon
+	        Donjon donjon = new Donjon();
 
-        // Proposition au joueur de choisir une pièce
-        System.out.println("Choisissez une pièce où entrer (de 1 à 8) : ");
-        int choix = scan.nextInt();
+	        // Proposition au joueur de choisir une pièce
+	        Printer.printMessage("Choisissez une pièce où entrer (de 1 à 8) : ");
+	        int choix = scan.nextInt();
 
-        // Vérification de la validité du choix du joueur
-        if (choix >= 1 && choix <= 8) {
-            donjon.entrerPiece(choix);
-        } else {
-            System.out.println("Choix invalide. Fin du programme.");
-        }
+	        // Vérification de la validité du choix du joueur
+	        if (choix >= 1 && choix <= 8) {
+	            donjon.entrerPiece(choix);
+	        } else {
+	            Printer.printMessage("Choix invalide. Fin du programme.");
+	        }
 
-        scan.close();
-    }
-}
+	        scan.close();
+	    }
+	}

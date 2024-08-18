a76e7d125ec84288014240f10c33f2528b7f0180
Longuevville <mrlongueville@proton.me>
2024-08-18 19:54:05 +0200

    correction bugs newbie level

---
+++ donjonquest/Printer.java
@@ -0,0 +1,29 @@
+package donjonquest;
+
+public class Printer {
+
+
+
+		// Affichage standard
+
+		public static void printMessage(String message) {
+			System.out.println(message);
+
+		}
+
+		// Affiché un message avec un titre
+
+		public static void printWithTitle(String title, String message) {
+			System.out.println(title + ": " + message);
+		}
+
+		// pause question
+
+		public static void askQuestion(String question) {
+			System.out.println(question);
+		}
+
+	}
+
+
+
